ead0498f129dd14aaca3a75c1ba6fb45dabfb0fd
carlosgg00 <garciaariel@unah.hn>
2023-05-16 21:26:30 -0600

    Agregar archivos de proyecto.

---
+++ ContactsAPI/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactsAPI.Models
+{   //13.15
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Adress { get; set; }
+
+
+    }
+}
